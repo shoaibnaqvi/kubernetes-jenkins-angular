93f37bd0d4a2720ab087b96c3b416fdf25cfd367
Syed Shoaib <shoaib.icup@gmail.com>
2022-12-20 23:06:23 -0500

    changes script for initial setup

--- setup.sh
+++ setup.sh
@@ -12,38 +12,27 @@ function setup_jenkins() {
 
   #setup jenkins
   cd $PROJECT_ROOT
-  if [ -d "$PROJECT_ROOT/../jenkins" ]; then
-    echo "jenkins repository already exists."
-    cd ../jenkins
-    echo "inside jenkins directory: $(pwd)"
-    git fetch --all && git pull && ./install.sh
-  else
-    cd ..
-    echo "Cloning jenkins"
-    git clone git@github.com:shoaibnaqvi/jenkins.git && echo "cloned jenkins" && cd "jenkins" && ./install.sh
-  fi
+  cd ..
+  rm -rf jenkins
+  echo "Cloning jenkins"
+  git clone git@github.com:shoaibnaqvi/jenkins.git && echo "cloned jenkins" && cd "jenkins" && ./install.sh
 }
 
 function setup_frontend() {
   cd $PROJECT_ROOT
-  if [ -d "$PROJECT_ROOT/frontend" ]; then
-    echo "frontend repository already exists."
-    cd ../frontend
-    echo "inside frontend directory: $(pwd)"
-    git fetch --all && git pull
-  else
-    cd ..
-    echo "Cloning frontend"
-    git clone git@github.com:shoaibnaqvi/frontend.git && echo "cloned frontend" && cd "frontend" && npm install
-  fi
-
+  cd ..
+  rm -rf frontend
+  echo "Cloning frontend"
+  git clone git@github.com:shoaibnaqvi/frontend.git && echo "cloned frontend" && cd "frontend" && npm install
+  : '
   if lsof -t -i:4200 >/dev/null; then
     echo "Already running frontend found"
     sudo kill $(sudo lsof -t -i:4200)
   else
     echo "Port is free. Starting frontend"
   fi
-  ng serve --host 192.168.0.134 & disown
+  ng serve --host 192.168.0.134
+  '
 }
 
 #setup kubernetes
